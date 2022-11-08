d425d5dcbebba21b16f69eb2df7f98b7bdb430d0
Diana-Verevkina <diva2208@mail.ru>
2022-11-08 17:15:35 +0300

    send on second review

--- api_yamdb/reviews/management/commands/data_loading.py
+++ api_yamdb/reviews/management/commands/data_loading.py
@@ -1,4 +1,4 @@
-# import csv
+import csv
 
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -7,6 +7,43 @@ from reviews.models import (Category, Genre, Title, TitlesGenre, Review, User,
                             Comment)
 
 path = f'{settings.BASE_DIR}/static/data'
+
+
+def loading(model, row):
+    # Знаю, что убого, по-другому пока не придумала
+    if model == Category:
+        columns = Category(id=row['id'], name=row['name'],
+                           slug=row['slug'])
+    elif model == Genre:
+        columns = Genre(id=row['id'], name=row['name'],
+                        slug=row['slug'])
+    elif model == Title:
+        columns = Title(id=row['id'], name=row['name'],
+                        year=row['year'],
+                        category_id=row['category'])
+    elif model == TitlesGenre:
+        columns = TitlesGenre(id=row['id'],
+                              titles_id=row['title_id'],
+                              genre_id=row['genre_id'])
+    elif model == Review:
+        columns = Review(
+            id=row['id'], title_id=row['title_id'],
+            text=row['text'], author_id=row['author'],
+            score=row['score'], pub_date=row['pub_date'])
+    elif model == User:
+        columns = User(
+            id=row['id'], username=row['username'],
+            email=row['email'], role=row['role'],
+            bio=row['bio'], first_name=row['first_name'],
+            last_name=row['last_name'],
+        )
+    elif model == Comment:
+        columns = Comment(
+            id=row['id'], review_id=row['review_id'],
+            text=row['text'], author_id=row['author'],
+            pub_date=row['pub_date']
+        )
+    return columns
 
 
 class Command(BaseCommand):
@@ -22,7 +59,7 @@ class Command(BaseCommand):
         Comment: 'comments',
     }
 
-    """def handle(self, *args, **options):
+    def handle(self, *args, **options):
         for model, file in self.model_csv.items():
             if model.objects.exists():
                 raise Exception(f'Ошибка. Данные в модель {model.__name__} '
@@ -30,39 +67,6 @@ class Command(BaseCommand):
             else:
                 print(f'Загрузка данных в модель {model.__name__}')
                 for row in csv.DictReader(open(f'{path}/{file}.csv')):
-                    # Знаю, что убого, по-другому пока не придумала
-                    if model == Category:
-                        columns = Category(id=row['id'], name=row['name'],
-                                           slug=row['slug'])
-                    elif model == Genre:
-                        columns = Genre(id=row['id'], name=row['name'],
-                                        slug=row['slug'])
-                    elif model == Title:
-                        columns = Title(id=row['id'], name=row['name'],
-                                        year=row['year'],
-                                        category_id=row['category'])
-                    elif model == TitlesGenre:
-                        columns = TitlesGenre(id=row['id'],
-                                              titles_id=row['title_id'],
-                                              genre_id=row['genre_id'])
-                    elif model == Review:
-                        columns = Review(
-                            id=row['id'], title_id=row['title_id'],
-                            text=row['text'], author_id=row['author'],
-                            score=row['score'], pub_date=row['pub_date'])
-                    elif model == User:
-                        columns = User(
-                            id=row['id'], username=row['username'],
-                            email=row['email'], role=row['role'],
-                            bio=row['bio'], first_name=row['first_name'],
-                            last_name=row['last_name'],
-                        )
-                    elif model == Comment:
-                        columns = Comment(
-                            id=row['id'], review_id=row['review_id'],
-                            text=row['text'], author_id=row['author'],
-                            pub_date=row['pub_date']
-                        )
-
+                    columns = loading(model, row)
                     columns.save()
-                print(f'Данные в модель {model.__name__} загружены')"""
+                print(f'Данные в модель {model.__name__} загружены')
