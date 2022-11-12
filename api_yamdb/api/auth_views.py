e0c3cf471bef854c86435fcbfb4398475c986609
Diana-Verevkina <diva2208@mail.ru>
2022-11-13 00:25:41 +0300

    3 review

--- api_yamdb/api/auth_views.py
+++ api_yamdb/api/auth_views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework_simplejwt.tokens import AccessToken
 
-from .serializers import RegisterDataSerializer, TokenSerializerпше
+from .serializers import RegisterDataSerializer, TokenSerializer
 from reviews.models import User
 
 msg_email_exists = 'Электронная почта уже занята!'
